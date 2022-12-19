8791dc4b9f9e3b340c7fb21d2b147f679fce589c
PaulaDura <117785037+PaulaDura@users.noreply.github.com>
2022-12-18 22:16:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function btnChanger1() {
+    let bg = document.getElementById("container").style.background = "fuchsia";
+}
+
+function btnChanger2() {
+    let bg = document.getElementById("container").style.background = "black";
+}
+
+function btnChanger3() {
+    let bg = document.getElementById("container").style.background = "coral";
+}
